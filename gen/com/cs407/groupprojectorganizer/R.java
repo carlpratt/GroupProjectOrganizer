d30f5799e33fdf480de5640602c20d439a52b92b
Carlo Pratt <carlo@mumble-35.cs.wisc.edu>
2014-03-10 13:44:34 -0500

    Added create_account layout

--- gen/com/cs407/groupprojectorganizer/R.java
+++ gen/com/cs407/groupprojectorganizer/R.java
@@ -19,23 +19,41 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int createbutton=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int button=0x7f080005;
-        public static final int button2=0x7f080006;
-        public static final int editText=0x7f080002;
+        public static final int action_settings=0x7f080015;
+        public static final int button=0x7f080011;
+        public static final int button1=0x7f08000d;
+        public static final int button2=0x7f080012;
+        public static final int checkBox1=0x7f080013;
+        public static final int checkBox2=0x7f080014;
+        public static final int editText=0x7f080010;
+        public static final int editText1=0x7f080002;
         public static final int editText2=0x7f080004;
-        public static final int textView=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int editText3=0x7f080006;
+        public static final int editText4=0x7f080008;
+        public static final int editText5=0x7f08000a;
+        public static final int editText6=0x7f08000c;
+        public static final int imageView1=0x7f080000;
+        public static final int textView=0x7f08000f;
+        public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f080007;
+        public static final int textView5=0x7f080009;
+        public static final int textView6=0x7f08000b;
+        public static final int textView7=0x7f08000e;
     }
     public static final class layout {
-        public static final int createnew=0x7f030000;
-        public static final int login=0x7f030001;
+        public static final int create_account=0x7f030000;
+        public static final int createnew=0x7f030001;
+        public static final int editprofile=0x7f030002;
+        public static final int login=0x7f030003;
+        public static final int settings=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -43,7 +61,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int create_account=0x7f050009;
+        public static final int email=0x7f050005;
+        public static final int first_name=0x7f050006;
         public static final int hello_world=0x7f050002;
+        public static final int last_name=0x7f050007;
+        public static final int password=0x7f050004;
+        public static final int retype_password=0x7f050008;
+        public static final int username=0x7f050003;
     }
     public static final class style {
         /** 
