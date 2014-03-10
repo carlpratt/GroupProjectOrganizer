a1c034d72c852d49adbd9bf19e9a4024f81f31be
Carlo Pratt <carlo@mumble-35.cs.wisc.edu>
2014-03-10 13:54:28 -0500

    Added projects layout for viewing a list of created projects

--- gen/com/cs407/groupprojectorganizer/R.java
+++ gen/com/cs407/groupprojectorganizer/R.java
@@ -25,12 +25,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080015;
+        public static final int action_settings=0x7f080016;
         public static final int button=0x7f080011;
         public static final int button1=0x7f08000d;
         public static final int button2=0x7f080012;
-        public static final int checkBox1=0x7f080013;
-        public static final int checkBox2=0x7f080014;
+        public static final int checkBox1=0x7f080014;
+        public static final int checkBox2=0x7f080015;
         public static final int editText=0x7f080010;
         public static final int editText1=0x7f080002;
         public static final int editText2=0x7f080004;
@@ -39,6 +39,7 @@ public final class R {
         public static final int editText5=0x7f08000a;
         public static final int editText6=0x7f08000c;
         public static final int imageView1=0x7f080000;
+        public static final int listView1=0x7f080013;
         public static final int textView=0x7f08000f;
         public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080003;
@@ -53,13 +54,15 @@ public final class R {
         public static final int createnew=0x7f030001;
         public static final int editprofile=0x7f030002;
         public static final int login=0x7f030003;
-        public static final int settings=0x7f030004;
+        public static final int projects=0x7f030004;
+        public static final int settings=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_new_project=0x7f05000a;
         public static final int app_name=0x7f050000;
         public static final int create_account=0x7f050009;
         public static final int email=0x7f050005;
