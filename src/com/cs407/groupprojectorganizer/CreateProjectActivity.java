e3d3a56a993ad8e85ee38dec0910cfe831d30ae0
Carl Pratt <carl.pratt.13@gmail.com>
2014-04-11 17:56:49 -0500

    Stopped keyboard from auto-launching on new activities, and made modifiecations to the edit profile and create new project layouts.

--- src/com/cs407/groupprojectorganizer/CreateProjectActivity.java
+++ src/com/cs407/groupprojectorganizer/CreateProjectActivity.java
@@ -1,7 +1,9 @@
 package com.cs407.groupprojectorganizer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateProjectActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class CreateProjectActivity extends Activity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_new_project);
+    }
+
+    public void onButtonClick(View view){
+
+        switch (view.getId()){
+            case R.id.btnCreateNewProject:
+                finish();
+                break;
+        }
     }
 }
