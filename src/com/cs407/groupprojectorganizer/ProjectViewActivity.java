f4f4ce54d4dac9e4b88f8926a36845388dc4682c
Carl Pratt <carl.pratt.13@gmail.com>
2014-04-26 18:23:27 -0500

    Added logging statements to show incorrect pids being passed to ProjectViewActivity

--- src/com/cs407/groupprojectorganizer/ProjectViewActivity.java
+++ src/com/cs407/groupprojectorganizer/ProjectViewActivity.java
@@ -46,6 +46,8 @@ public class ProjectViewActivity extends Activity {
         proj.setText(project_title.get(position));
         desc.setText(project_desc.get(position));
         pid = pids.get(position);
+
+        Log.d("pid set in onCreate", pid);
     }
 
     public void onButtonClick(View view){
@@ -97,7 +99,7 @@ public class ProjectViewActivity extends Activity {
             List<NameValuePair> params = new ArrayList<NameValuePair>();
             params.add(new BasicNameValuePair("pid", pid));
 
-            Log.d("pid", pid);
+            Log.d("pid of deleted project", pid);
 
             // getting JSON Object
             JSONObject json = jsonParser.makeHttpRequest(url_delete_project,
